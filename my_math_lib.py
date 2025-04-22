056fb5a66a2c9497d82d0e8c427af3b0b0516c60
fakhirsh <fakhir.shaheen@gmail.com>
2025-04-22 08:28:55 +0500

    initial commit

---
+++ my_math_lib.py
@@ -0,0 +1,13 @@
+# File: my_math_lib.py
+"""
+A simple math library for demonstrating Git workflow.
+Contains basic mathematical operations.
+"""
+
+def add(a, b):
+    """Add two numbers and return the result."""
+    return a + b
+
+def subtract(a, b):
+    """Subtract b from a and return the result."""
+    return a - b
